e7b64ecf67ca4002c6a43a28716aa703ef2d3a64
Keita Iwasa <keita030909@gmail.com>
2024-02-09 23:28:49 +0900

    initial commit

---
+++ my-node-app/index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const {Datastore} = require('@google-cloud/datastore');
+const app = express();
+const port = 8080;
+
+// Google Cloud Datastoreのインスタンスを初期化
+const datastore = new Datastore();
+
+app.use(express.json());
+
+app.post('/data', async (req, res) => {
+    const { temperature, humidity } = req.body; // リクエストボディからデータを抽出
+    console.log(`Received temperature: ${temperature}, humidity: ${humidity}`);
+
+    const kind = 'SensorData'; // Datastoreのエンティティタイプ
+    const dataKey = datastore.key([kind]);
+    const entity = {
+        key: dataKey,
+        data: [
+            { name: 'temperature', value: temperature },
+            { name: 'humidity', value: humidity },
+            { name: 'timestamp', value: new Date() } // タイムスタンプを追加
+        ],
+    };
+
+    try {
+        await datastore.save(entity);
+        res.status(200).send('Data received and stored.');
+    } catch (error) {
+        console.error('Error saving data:', error);
+        res.status(500).send('Failed to save data.');
+    }
+});
+
+
+app.get('/data', async (req, res) => {
+    const query = datastore.createQuery('SensorData').order('timestamp', {descending: true});
+    try {
+        const [entities] = await datastore.runQuery(query);
+        const data = entities.map(entity => {
+            const dataObject = {
+                id: entity[datastore.KEY].id,
+                temperature: entity.temperature,
+                humidity: entity.humidity,
+                timestamp: entity.timestamp
+            };
+            return dataObject;
+        });
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Failed to fetch data');
+    }
+});
+
+app.get('/dashboard', async (req, res) => {
+    const query = datastore.createQuery('SensorData').order('timestamp', {descending: true}).limit(10);
+    try {
+        const [entities] = await datastore.runQuery(query);
+        let htmlResponse = '<h1>Temperature and Humidity Data</h1>';
+        htmlResponse += '<ul>';
+        entities.forEach(entity => {
+            htmlResponse += `<li>Temperature: ${entity.temperature}, Humidity: ${entity.humidity}, Timestamp: ${entity.timestamp}</li>`;
+        });
+        htmlResponse += '</ul>';
+        res.send(htmlResponse);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Failed to fetch data');
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ sketch_esp32_2/sketch_esp32_2.ino
@@ -0,0 +1,65 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <DHT.h>
+
+// DHTセンサーの設定
+#define DHTPIN 14     // ESP32の接続ピン
+#define DHTTYPE DHT11 // DHT 11を使用
+DHT dht(DHTPIN, DHTTYPE);
+
+
+const char* ssid = "aterm-4d09d9-g";
+const char* password = "1fde8dda40d72";
+const char* serverName = "http://192.168.10.105:8080/data";
+
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  dht.begin();
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    // センサーの読み取りが成功しているか確認
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    HTTPClient http;
+
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
+
+    // JSONデータの作成
+    String httpRequestData = "{\"temperature\":" + String(t) + ", \"humidity\":" + String(h) + "}";
+    
+    int httpResponseCode = http.POST(httpRequestData);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.print(httpResponseCode);
+      Serial.print(response);
+      Serial.println(httpRequestData);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  } else {
+    Serial.println("Error in WiFi connection");
+  }
+
+  delay(10000); // 30秒ごとにデータを送信
+}
+
