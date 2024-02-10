b24bcafa50bba408fcec4d9f9f94a01e934d3fc9
Keita Iwasa <keita030909@gmail.com>
2024-02-10 14:01:46 +0900

    timestamp

--- my-node-app/index.js
+++ my-node-app/index.js
@@ -19,6 +19,7 @@ app.post('/data', async (req, res) => {
         data: [
             { name: 'temperature', value: temperature },
             { name: 'humidity', value: humidity },
+            { name: 'timestamp', value: new Date() } // タイムスタンプを追加
         ],
     };
 
