e36e30454f345fa90b6aacb6f237474187456444
Keita Iwasa <keita030909@gmail.com>
2024-03-17 18:32:45 +0900

    Felica対応

--- sketch_NFC/sketch_NFC.ino
+++ sketch_NFC/sketch_NFC.ino
@@ -1,31 +1,65 @@
-#include <Wire.h>
-#include <PN532_I2C.h>
+#include <PN532_SWHSU.h>
 #include <PN532.h>
-#include <NfcAdapter.h>
-PN532_I2C pn532_i2c(Wire);
-NfcAdapter nfc = NfcAdapter(pn532_i2c);
-String tagId = "None";
-byte nuidPICC[4];
- 
-void setup(void) 
-{
- Serial.begin(115200);
- Serial.println("System initialized");
- nfc.begin();
+#include <SoftwareSerial.h>
+
+SoftwareSerial softSerial(2, 3); // RX, TX
+PN532_SWHSU pn532swhsu(softSerial);
+PN532 nfc(pn532swhsu);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello!");
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (!versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
+  
+  // Set the max number of retry attempts to read from a card
+  // This prevents us from waiting forever for a card, which is
+  // the default behaviour of the PN532.
+  nfc.setPassiveActivationRetries(0xFF);
+  
+  // Configure board to read RFID tags
+  nfc.SAMConfig();
+  
+  Serial.println("Waiting for an Felica card");
 }
- 
-void loop() 
-{
- readNFC();
-}
- 
-void readNFC() 
-{
- if (nfc.tagPresent())
- {
-   NfcTag tag = nfc.read();
-   tag.print();
-   tagId = tag.getUidString();
- }
- delay(5000);
+
+void loop() {
+  uint8_t idm[] = { 0, 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the IDm (8 bytes)
+  uint8_t pmm[] = { 0, 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the PMm (8 bytes)
+  uint16_t systemCode = 0xFFFF;                // Wildcard system code (0xFFFF)
+  uint16_t systemCodeResponse;
+  
+  // Wait for a FeliCa card (including Mobile Suica)
+  int8_t result = nfc.felica_Polling(systemCode, 0x00, idm, pmm, &systemCodeResponse);
+  
+  if (result == 1) {
+    Serial.println("Mobile Suica detected!");
+    
+    // Print the IDm of the Mobile Suica
+    Serial.print("IDm: ");
+    for (uint8_t i = 0; i < 8; i++) {
+      Serial.print(idm[i] < 0x10 ? " 0" : " ");
+      Serial.print(idm[i], HEX);
+    }
+    Serial.println();
+    
+    // Wait 1 second before continuing
+    delay(1000);
+  }
+  else
+  {
+    // PN532 probably timed out waiting for a card
+    delay(800);
+  }
 }
