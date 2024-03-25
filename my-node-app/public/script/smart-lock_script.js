2b163fd90ed570749e22ed4685f392937aa50309
Keita Iwasa <keita030909@gmail.com>
2024-03-26 01:43:18 +0900

    first comit frontend

---
+++ my-node-app/public/script/smart-lock_script.js
@@ -0,0 +1,6 @@
+document.getElementById('unlockButton').addEventListener('click', function() {
+    console.log('Unlock button clicked');
+    // Here you would add the code to update the lock's status
+    // For example, by sending a request to your Firebase Firestore
+    alert('Unlock request sent!');
+});
