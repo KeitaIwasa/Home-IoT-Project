d0b948a8de1b2c4d5877341d6f91ce7e066bd5f4
Keita Iwasa <keita030909@gmail.com>
2024-03-27 23:38:57 +0900

    タクトスイッチでもunLock

--- sketch_Receiver/sketch_Receiver.ino
+++ sketch_Receiver/sketch_Receiver.ino
@@ -4,6 +4,8 @@
 
 Stepper sm(2048, 32, 25, 33, 26);
 
+const int TACT_SWITCH_PIN = 27;
+
 // Structure to receive data
 typedef struct {
   bool cardDetected;
@@ -23,24 +25,34 @@ void setup() {
   esp_now_register_recv_cb(OnDataRecv);
 
   sm.setSpeed(10);
+  pinMode(TACT_SWITCH_PIN, INPUT_PULLUP);
 
 }
 
 void loop() {
+  if (digitalRead(TACT_SWITCH_PIN) == LOW) { // タクトスイッチが押されたかどうかを確認
+    unLock(); // モーターを動作させる関数を呼び出す
+    delay(3000);
+  }
 }
 
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
   memcpy(&packet, data, sizeof(packet));
-
   if (packet.cardDetected) {
-    Serial.println("Specific FeliCa card detected!");
-    sm.step((float)2048 * +90 / 360);
-    delay(1000);
-    sm.step((float)2048 * -90 / 360);
-    // モーターの動作が完了した直後にピンをLOWにする
-    digitalWrite(32, LOW);
-    digitalWrite(25, LOW);
-    digitalWrite(33, LOW);
-    digitalWrite(26, LOW);  
+    unLock(); 
+    delay(3000);
   }
+}
+
+void unLock() {
+  Serial.println("Door unlocked");
+  sm.step((float)2048 * +90 / 360);
+  delay(1000);
+  sm.step((float)2048 * -90 / 360);
+  delay(100);
+  // モーターの動作が完了した直後にピンをLOWにする
+  digitalWrite(32, LOW);
+  digitalWrite(25, LOW);
+  digitalWrite(33, LOW);
+  digitalWrite(26, LOW);
 }
